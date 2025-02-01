35a9424f5bdeb2c36826985a8371ae729680e8d0
Ankit Raj <150550815+ankitraj0901@users.noreply.github.com>
2025-02-01 18:48:11 +0530

    Time: 0 ms (100.00%) | Memory: 40.2 MB (78.81%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -2,8 +2,16 @@ class Solution {
     public int fib(int n) {
         if(n==0)
             return 0;
-        if(n==1 || n==2) 
-            return 1;
-        return fib(n-1) + fib(n-2);
+        int a=0,b=1,x=1;
+        return fab(a,b,x,n);
+    }
+    public static int fab(int a,int b,int x,int n){
+        int c=0;
+        c=a+b;
+        a=b;
+        b=c;
+        if(x==n)
+            return a;
+        return fab(a, b, x+1, n);
     }
 }
