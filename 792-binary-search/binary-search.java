38206c643272d587df9610c168e224d67cc39365
Ankit Raj <150550815+ankitraj0901@users.noreply.github.com>
2025-03-05 02:06:27 +0530

    Time: 1 ms (10.57%) | Memory: 46.1 MB (22.83%) - LeetSync

---
+++ 792-binary-search/binary-search.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        if(nums.length==0)
+            return -1;
+        if(nums.length==1)
+            if(nums[0]==target)
+                return 0;
+            else
+                return -1;
+        int mid=nums.length/2;
+        if(target<nums[mid]){
+            for(int i=0;i<mid;i++){
+                if(nums[i]==target)
+                    return i;
+            }
+        }
+        else if(target>nums[mid]){
+            for(int j=mid+1;j<nums.length;j++){
+                if(nums[j]==target)
+                    return j;
+            }
+        }
+        else{
+            return mid;
+        }
+        return -1;
+    }
+}
