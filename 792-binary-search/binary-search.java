3e555bb3492ec4ac41e3afd6c720b61195671ba4
Ankit Raj <150550815+ankitraj0901@users.noreply.github.com>
2025-03-05 02:08:16 +0530

    Time: 1 ms (10.57%) | Memory: 45.7 MB (59.37%) - LeetSync

--- 792-binary-search/binary-search.java
+++ 792-binary-search/binary-search.java
@@ -1,7 +1,7 @@
 class Solution {
     public int search(int[] nums, int target) {
-        if(nums.length==0)
-            return -1;
+        // if(nums.length==0)
+        //     return -1;
         // if(nums.length==1)
         //     if(nums[0]==target)
         //         return 0;
