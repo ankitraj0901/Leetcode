85cfcfa314c8d9b2f01e4aa4eeb5b39296ca5109
Ankit Raj <150550815+ankitraj0901@users.noreply.github.com>
2025-03-09 10:13:32 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (64.70%) - LeetSync

---
+++ 118-pascals-triangle/pascals-triangle.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> list;
+    
+    for (int i = 0; i < numRows; i++) {
+        vector<int> temp;
+        
+        if (list.empty()) {
+            temp.push_back(1);
+            list.push_back(temp);
+        } else {
+            if (list[i - 1].size() > 1) {
+                temp.push_back(1);
+                
+                for (size_t k = 0; k < list[i - 1].size() - 1; k++) {
+                    int val = list[i - 1][k] + list[i - 1][k + 1];
+                    temp.push_back(val);
+                }
+                
+                temp.push_back(1);
+                list.push_back(temp);
+            } else {
+                temp.push_back(1);
+                temp.push_back(1);
+                list.push_back(temp);
+            }
+        }
+    }
+    
+    return list;
+    }
+};
