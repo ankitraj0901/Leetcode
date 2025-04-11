7367238e1514f6396ab4230282a79cd2d2b947ea
Ankit Raj <150550815+ankitraj0901@users.noreply.github.com>
2025-04-12 00:57:55 +0530

    Time: 29 ms (37.95%) | Memory: 41.7 MB (86.87%) - LeetSync

---
+++ 2998-count-symmetric-integers/count-symmetric-integers.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int countSymmetricIntegers(int low, int high) {
+        int i,j,count=0,len=0,temp=0,sum1=0,sum2=0,rem=0;
+        for(i=low;i<=high;i++){
+            temp=i;
+            len=0;
+            while(temp!=0){
+                len++;
+                temp=temp/10;
+            }
+            if(len%2==0){
+                sum1=0;
+                sum2=0;
+                temp=i;
+                for(j=len;j>=1;j--){
+                    if(j<=len/2){
+                        rem=temp%10;
+                        sum1=sum1+rem;
+                        temp=temp/10; 
+                    }
+                    else{
+                        rem=temp%10;
+                        sum2=sum2+rem;
+                        temp=temp/10; 
+                    }
+                       
+                }
+                if(sum1==sum2)
+                    count++;
+            }
+        }
+        return count;
+    }
+}
