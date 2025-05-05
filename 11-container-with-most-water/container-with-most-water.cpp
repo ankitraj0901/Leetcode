bf50aa98e072e226da6b60d8d79c5585e58b6184
Ankit Raj <150550815+ankitraj0901@users.noreply.github.com>
2025-05-05 19:44:11 +0530

    Time: 0 ms (100.00%) | Memory: 63.1 MB (12.52%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.cpp
+++ 11-container-with-most-water/container-with-most-water.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
     int maxArea(vector<int>& height) {
-        int i=0,j=(height.size()-1),breadth,area1=0,area2=0;
-        while(i<=height.size()-2 && j>=i+1){
-            if(i==0 && j==height.size()-1){
+        int i=0,j=(height.size()-1),breadth,area=0,oldArea=0;
+        while(i<=j){
+            // if(i==0 && j==height.size()-1){
+            // }
                 breadth=j-i;
-                area1=height[i]<=height[j]?breadth*height[i]:breadth*height[j];
+                area=height[i]<=height[j]?breadth*height[i]:breadth*height[j];
                 
-            }
-            else{
-                breadth=j-i;
-                if(height[i]<=height[j])
-                    area2=breadth*height[i];
-                else
-                    area2=breadth*height[j];
-                if(area1<area2)
-                    area1=area2;
-            }
+            // else{
+            //     breadth=j-i;
+            //     if(height[i]<=height[j])
+            //         area2=breadth*height[i];
+            //     else
+            //         area2=breadth*height[j];
+            //}
+            if(area>oldArea)
+                oldArea=area;
             if(height[i]<=height[j])
                 i++;
             else
                 j--;
         
         }
-        return area1;
+        return oldArea;
     }
 };
