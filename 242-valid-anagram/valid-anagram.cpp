ed52f5a0884cf21ef0cfb2ff0123b60d250e00fa
Ankit Raj <150550815+ankitraj0901@users.noreply.github.com>
2025-05-05 22:14:40 +0530

    Time: 8 ms (8.58%) | Memory: 9.7 MB (74.31%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        map <char,int> m;
+        int j=0;
+        for(int i=0;i<s.size();i++){
+            if(m.find(s[i])!=m.end())
+                m[s[i]]=m[s[i]]+1;
+            else
+                m[s[i]]=1;
+        }
+        for(auto p:m)
+            cout<<p.first<<p.second<<endl;
+        for(j=0;j<t.size();j++){
+            if(m.find(t[j])!=m.end() && m[t[j]]>0)
+                m[t[j]]-=1;
+            else
+                break;
+        }
+        if(j==t.size() && s.size()==t.size())
+            return true;
+        return false;
+    }
+};
