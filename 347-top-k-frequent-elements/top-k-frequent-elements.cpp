00960ee62355f866cead876b40564f8cc5abc128
Ankit Raj <150550815+ankitraj0901@users.noreply.github.com>
2025-05-09 22:28:33 +0530

    Time: 172 ms (5.04%) | Memory: 18.2 MB (25.01%) - LeetSync

---
+++ 347-top-k-frequent-elements/top-k-frequent-elements.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        map <int,int>m;
+        vector <int>v1;
+        for(int i=0;i<nums.size();i++){
+            if(m.find(nums[i])!=m.end()){
+                m[nums[i]]+=1; 
+            }
+            else
+                m[nums[i]]=1;
+        }
+        int temp=0,index=0;
+        map <int,bool>m2;
+        for(int j=0;j<k;j++){
+            temp=0;
+            for(auto e:m){
+                if(m2[e.first]==true)
+                    continue;
+                else{
+                    if(temp<e.second){
+                        temp=e.second;
+                        index=e.first;
+                        cout<<index;
+                    }
+                }
+            }
+            // if(index!=0){
+            // }
+                v1.push_back(index);
+                m2[index]=true;
+            
+        }
+        return v1;
+    }
+};
