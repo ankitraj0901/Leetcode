fdc257dd7fe5c24240871c621af1ed7040a1cd89
Ankit Raj <150550815+ankitraj0901@users.noreply.github.com>
2025-05-11 09:48:47 +0530

    Time: 224 ms (5.05%) | Memory: 91.2 MB (11.61%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -9,8 +9,6 @@ public:
             else
                 m[nums[i]]=i;
         }
-        for(auto e:m)
-            cout<<e.first<<":"<<e.second<<endl;
         int index=0,max=0,count=1;
         while(index<nums.size()){
             if(m.find(nums[index]+1)!=m.end()){
