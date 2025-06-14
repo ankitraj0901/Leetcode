caff6083b5223baf92e4f095a51ff053f6365e1c
Ankit Raj <150550815+ankitraj0901@users.noreply.github.com>
2025-06-14 19:02:52 +0530

    Time: 2 ms (27.58%) | Memory: 57.5 MB (35.84%) - LeetSync

--- 189-rotate-array/rotate-array.java
+++ 189-rotate-array/rotate-array.java
@@ -1,19 +1,26 @@
+import java.util.Vector;
 class Solution {
     public void rotate(int[] nums, int k) {
-        if (k % nums.length == 0)
-            return;
-        int i,j;
         int res = k % nums.length;
-        int round=nums.length-res;
-        int arr[]=new int[nums.length];
-        for (i = round, j=0; i < nums.length; i++, j++){
-            arr[j] = nums[i];
+        int j = nums.length-1;
+        int temp;
+        for (int i = 0; i < j; i++, j--){
+            temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
         }
-        System.out.println(j);
-        for (int m = 0; m < round; m++,j++)
-            arr[j] = nums[m];
+        int split = nums.length - res;
 
-        for (int l = 0; l < nums.length; l++)
-            nums[l] = arr[l];
+        for (int a = 0, b = res-1; a < b; a++, b--){
+            temp = nums[a];
+            nums[a] = nums[b];
+            nums[b] = temp;
+        }
+
+        for (int a = res, b = nums.length-1; a < b; a++, b--){
+            temp = nums[a];
+            nums[a] = nums[b];
+            nums[b] = temp;
+        }
     }
 }
